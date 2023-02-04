37cb1ae2eb1a4b499793522a69e880020db92a2e
nbobic1 <nbobic1@etf.unsa.ba>
2023-02-04 20:28:37 +0100

    Initial Commit

---
+++ tzpi 2/tzpi_2App.swift
@@ -0,0 +1,17 @@
+//
+//  tzpi_2App.swift
+//  tzpi 2
+//
+//  Created by sova on 4. 2. 2023..
+//
+
+import SwiftUI
+
+@main
+struct tzpi_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
